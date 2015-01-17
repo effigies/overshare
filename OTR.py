7957203a22b9c25b5698baa25dba2578e60e5221
Christopher J. Johnson <effigies@gmail.com>
2015-01-17 16:43:50 -0500

    DOC OTR.py documentation

--- OTR.py
+++ OTR.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Handler for OTRv3-conforming keys"""
 from hashlib import sha1
 from base64 import b64encode, b64decode
 from spec import SHORT, INT, MPI
@@ -12,8 +13,11 @@ class PublicKey:
 
     Subclasses are responsible for defining a parameter sequence to be
     hashed for fingerprints"""
+
     @property
     def fingerprint(self):
+        """The fingerprint is the SHA1 hash of the concatenated public key
+        parameters."""
         # DSA keys should not have keytype, for backward compatibility
         if self.keytype == 0:
             seq = self.params
@@ -24,17 +28,29 @@ class PublicKey:
             keypart.bytestring() for keypart in seq)).digest(), 'big'))
 
     def bytes(self):
+        """A bytestring representation of a public key: the key type code,
+        followed by the key parameters."""
         seq = [self.keytype] + self.params
         return b''.join(keypart.bytestring() for keypart in seq)
 
     def __str__(self):
+        """Base-64 encoding of the public key.
+
+        The following assertions should hold:
+            str(PublicKey.fromBase64(<ASCII>)) == <ASCII>
+            PublicKey.fromBase64(str(<KEYOBJ>)) == <KEYOBJ>
+        """
         return b64encode(self.bytes()).decode()
 
     def __repr__(self):
+        """Represent a key as its class name and fingerprint"""
         return '<{} {}>'.format(self.__class__.__name__, self.fingerprint)
 
     @classmethod
     def fromBase64(cls, string):
+        """Determine keytype from first two bytes, and allow the subclass to
+        interpret the key"""
+
         bs = b64decode(string.encode())
         classcode = KEYCODES[SHORT.from_bytes(bs[:2], 'big')]
         if cls == PublicKey:
@@ -46,21 +62,59 @@ class PublicKey:
 
 
 class PublicDSA(PublicKey):
+    """
+    OTR public authentication DSA key (PUBKEY):
+
+        Pubkey type (SHORT)
+            DSA public keys have type 0x0000
+
+        p (MPI)
+        q (MPI)
+        g (MPI)
+        y (MPI)
+
+            (p,q,g,y) are the DSA public key parameters
+
+    Quoted from: https://otr.cypherpunks.ca/Protocol-v3-4.0.0.html
+    """
     keytype = SHORT(0x0000)
 
     def __init__(self, p, q, g, y):
+        """Construct DSA public key from p, q, g, y parameters"""
         self.p, self.q, self.g, self.y = map(MPI, (p, q, g, y))
 
     @property
     def params(self):
+        """(p,q,g,y) are the DSA public key parameters"""
         return [self.p, self.q, self.g, self.y]
 
     @classmethod
     def fromSExpression(cls, expr):
+        """Interpret S-expression representation of public key, such
+        as that used in libpurple.
+
+        Example:
+            (dsa
+                (p <PVALUE>)
+                (q <QVALUE>)
+                (g <GVALUE>)
+                (y <YVALUE>)
+                [OPTIONAL FURTHER VALUES...]
+            )
+        """
         return cls(expr[0]['p'], expr[1]['q'], expr[2]['g'], expr[3]['y'])
 
     @classmethod
     def fromBase64(cls, string):
+        """Decode base-64 encoded DSA public key.
+
+        Input should be keycode, followed by MPI representation of p, q, g
+        and y.
+
+        MPI representation is a 4-byte length field, followed by the number
+        of bytes in that field.
+
+        All values are big-endian."""
         bs = b64decode(string.encode())
 
         keycode = SHORT.from_bytes(bs[:2], 'big')
@@ -88,11 +142,27 @@ class PublicDSA(PublicKey):
 
 
 class PrivateDSA(PublicDSA):
+    """Private DSA keys contain the same information as public keys, but with
+    a secret parameter"""
     def __init__(self, p, q, g, y, x):
+        """Construct DSA private key from p, q, g, y and x parameters"""
         self.p, self.q, self.g, self.y, self.x = map(MPI, (p, q, g, y, x))
 
     @classmethod
     def fromSExpression(cls, expr):
+        """Interpret S-expression representation of public key, such
+        as that used in libpurple.
+
+        Example:
+            (dsa
+                (p <PVALUE>)
+                (q <QVALUE>)
+                (g <GVALUE>)
+                (y <YVALUE>)
+                (x <XVALUE>)
+                [OPTIONAL FURTHER VALUES...]
+            )
+        """
         return cls(expr[0]['p'], expr[1]['q'], expr[2]['g'], expr[3]['y'],
                    expr[4]['x'])
 
